cbfd067990659f427aa0b0b283d5f6dff7938c17
brianschnee <brian.a.schnee@gmail.com>
2022-07-20 17:57:31 -0400

    fixed spacing

--- easy/412-fizz_buzz.js
+++ easy/412-fizz_buzz.js
@@ -12,8 +12,7 @@
 var fizzBuzz = function(n) {
     let ans = [];
     
-    for (let i = 1; i <= n; i++) {
-        
+    for (let i = 1; i <= n; i++) {   
         if(i % 15 === 0) {
             ans.push('FizzBuzz');
         } else if (i % 3 === 0) {
