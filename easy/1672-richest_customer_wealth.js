55339a7528eeaa4da1b2aaaa515efead4efcf7c7
brianschnee <brian.a.schnee@gmail.com>
2022-07-19 20:13:06 -0400

    first commit

---
+++ easy/1672-richest_customer_wealth.js
@@ -0,0 +1,23 @@
+/**
+ * Name: Richest Customer Wealth
+ * Description: You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+ * Link: https://leetcode.com/problems/richest-customer-wealth/
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = 0;
+    let current = 0;
+    
+    for(let i = 0; i < accounts.length; i++) {
+        for(let j = 0; j < accounts[i].length; j++) {
+            current += accounts[i][j];
+        }
+        if(current > max) {
+            max = current;
+        }
+        current = 0;
+    }
+    
+    return max;
+};
